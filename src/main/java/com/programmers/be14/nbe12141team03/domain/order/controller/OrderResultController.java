fc17108bed2ee580338551044642cf68c7f16f8b
Lemnoideae <aquan5584j@gmail.com>
2026-08-25 16:30:38 +0900

    annotation 관련 import 구문 수정(*로 변경)

--- src/main/java/com/programmers/be14/nbe12141team03/domain/order/controller/OrderResultController.java
+++ src/main/java/com/programmers/be14/nbe12141team03/domain/order/controller/OrderResultController.java
@@ -10,12 +10,7 @@ import com.programmers.be14.nbe12141team03.global.dto.RsData;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
