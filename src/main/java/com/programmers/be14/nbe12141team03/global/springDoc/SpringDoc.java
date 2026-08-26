c5b888b37eb063e8d1fb5ceb1aa2d0dbaee9e30a
HunheeChoi <67158609+hunhee99@users.noreply.github.com>
2026-08-26 10:34:42 +0900

    API 문서 개선

--- src/main/java/com/programmers/be14/nbe12141team03/global/springDoc/SpringDoc.java
+++ src/main/java/com/programmers/be14/nbe12141team03/global/springDoc/SpringDoc.java
@@ -2,15 +2,22 @@ package com.programmers.be14.nbe12141team03.global.springDoc;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(
-        title = "Grids & Circles API",
-        description = "카페 원두 주문 서비스 API 문서입니다."
-))
+@OpenAPIDefinition(
+        info = @Info(
+            title = "Grids & Circles API",
+            description = "카페 원두 주문 서비스 API 문서입니다."
+        ),
+        tags = {
+                @Tag(name = "관리자", description = "관리자 전용 API"),
+                @Tag(name = "공용", description = "고객용 API")
+        }
+)
 public class SpringDoc {
 
     @Bean
