3c1397116b988c5d42a52d02ed98391d913c0932
Deokwoo Kim <ejrdn7298@gmail.com>
2026-08-26 17:34:36 +0900

    상품 조회 api연동

--- frontend/src/app/(customer)/orders/page.tsx
+++ frontend/src/app/(customer)/orders/page.tsx
@@ -1,5 +1,49 @@
 "use client";
 
-export default function CustomerOrderPage(){
-    return <div>[고객]주문</div>
+import { useEffect, useState } from "react";
+import type { ProductResponse } from "@/types/product";
+import type { RsData } from "@/types/order";
+import PageHeader from "@/components/common/PageHeader";
+
+export default function CustomerOrderPage() {
+  const [products, setProducts] = useState<ProductResponse[]>([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch(
+          "http://localhost:8080/api/products"
+        );
+
+        if (!response.ok) {
+          throw new Error("상품 목록을 불러오지 못했습니다.");
+        }
+
+        const result: RsData<ProductResponse[]> =
+          await response.json();
+
+        setProducts(result.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchProducts();
+  }, []);
+
+  return (
+  <>
+    <PageHeader
+      title="상품 목록"
+      description="여러 상품을 담아 한 번에 주문합니다."
+      actions={
+        <div className="border rounded bg-white px-3 py-2 font-monospace">
+          GET /api/products
+        </div>
+      }
+    />
+
+    <div>상품 개수: {products.length}</div>
+  </>
+);
 }
